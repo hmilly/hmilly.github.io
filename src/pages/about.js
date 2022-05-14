498d74a0cf26676d61ad282f2ae5ce187e3ad270
hmilly <milstowers@gmail.com>
2022-05-14 01:15:28 +0100

    updated paras

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,13 +13,13 @@ const AboutPage = () => {
           <p>Hi! Thanks for visiting!</p>
           <p>
             I have been interested in web design since myspace was around, but
-            have only taken to studying web development seriously in the last 2
-            years.
+            have only taken to studying web development seriously in the last
+            few years.
           </p>
           <p>
-            I have been focusing mostly on Frontend development, but have also
-            completed a backend course and it's definetely something I want to
-            spend more time with in the future.
+            I've mainly taken to working with (and around){" "}
+            <strong>React</strong>, I'm now looking to find a position which is
+            right for me, so I can be around all things tech all day long.
           </p>
           <p>
             Please feel free to view my projects, or reach out using the links
@@ -29,8 +29,7 @@ const AboutPage = () => {
           <h1>Things I want to learn more about:</h1>
           <p>Svelte</p>
           <p>Next.js</p>
-          <p>vue</p>
-          <p>backend development</p>
+          <p>Backend development</p>
         </div>
         <StackCard />
       </About>
